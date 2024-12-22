1ab672935cef06dcc662e28d0e8f3218af6acb6a
asmaa-204 <asmaa.abozaid204@gmail.com>
2024-12-22 17:28:43 +0200

    Make the ripple carry adder generic

--- Adders/RTL/rippleCarryAdder.v
+++ Adders/RTL/rippleCarryAdder.v
@@ -1,35 +1,33 @@
-module rippleCarryAdder(
-    input [31:0] a, b,
-    input cin,
-    output [31:0] sum,
-    output cout,
-    output overflow
+module rippleCarryAdder #(
+    parameter WIDTH = 32
+) (
+    input  [WIDTH-1:0] a, b,
+    input              cin,
+    output [WIDTH-1:0] sum,
+    output             cout,
+    output             overflow
 );
-    wire [31:0] carry;
-    wire [31:0] a_signed, b_signed;
-
-    assign a_signed = a;
-    assign b_signed = b;
+    wire [WIDTH-1:0] carry;
 
     genvar i;
     generate
-        for (i = 0; i < 32; i = i + 1) begin : RCA
+        for (i = 0; i < WIDTH; i = i + 1) begin : RCA
             if (i == 0) begin
                 fullAdder FA (
-                    .a(a_signed[i]), .b(b_signed[i]), .cin(cin),
+                    .a(a[i]), .b(b[i]), .cin(cin),
                     .sum(sum[i]), .cout(carry[i])
                 );
             end else begin
                 fullAdder FA (
-                    .a(a_signed[i]), .b(b_signed[i]), .cin(carry[i-1]),
+                    .a(a[i]), .b(b[i]), .cin(carry[i-1]),
                     .sum(sum[i]), .cout(carry[i])
                 );
             end
         end
     endgenerate
 
-    assign cout = carry[31];
+    assign cout = carry[WIDTH-1];
 
-    assign overflow = (a_signed[31] & b_signed[31] & ~sum[31]) |
-                      (~a_signed[31] & ~b_signed[31] & sum[31]);
+    assign overflow = (a[WIDTH-1] & b[WIDTH-1] & ~sum[WIDTH-1]) |
+                      (~a[WIDTH-1] & ~b[WIDTH-1] & sum[WIDTH-1]);
 endmodule
