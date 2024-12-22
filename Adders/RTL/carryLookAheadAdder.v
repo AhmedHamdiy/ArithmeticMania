9c335000fe226e59e91221906ef1da36fde712bc
asmaa-204 <asmaa.abozaid204@gmail.com>
2024-12-22 17:51:08 +0200

    Added carrySelectAdder (Tested)

--- Adders/RTL/carryLookAheadAdder.v
+++ Adders/RTL/carryLookAheadAdder.v
@@ -5,25 +5,26 @@ module carryLookAheadAdder (
     output [31:0] sum,
     output        cout,
     output        overflow
-);
+  );
 
-    wire [31:0] g;
-    wire [31:0] p;
-    wire [31:0] c;
+  wire [31:0] g;
+  wire [31:0] p;
+  wire [31:0] c;
 
-    assign g = a & b;
-    assign p = a ^ b;
+  assign g = a & b;
+  assign p = a ^ b;
 
-    assign c[0] = cin;
-    genvar i;
-    generate
-        for (i = 1; i < 32; i = i + 1) begin : carry_gen
-            assign c[i] = g[i-1] | (p[i-1] & c[i-1]);
-        end
-    endgenerate
+  assign c[0] = cin;
+  genvar i;
+  generate
+    for (i = 1; i < 32; i = i + 1)
+    begin : carry_gen
+      assign c[i] = g[i-1] | (p[i-1] & c[i-1]);
+    end
+  endgenerate
 
-    assign sum = p ^ c;
-    assign cout = g[31] | (p[31] & c[31]);
-    assign overflow = c[31] ^ cout;
+  assign sum = p ^ c;
+  assign cout = g[31] | (p[31] & c[31]);
+  assign overflow = c[31] ^ cout;
 
 endmodule
--- Adders/RTL/rippleCarryAdder.v
+++ Adders/RTL/rippleCarryAdder.v
@@ -1,33 +1,37 @@
 module rippleCarryAdder #(
     parameter WIDTH = 32
-) (
+  ) (
     input  [WIDTH-1:0] a, b,
     input              cin,
     output [WIDTH-1:0] sum,
     output             cout,
     output             overflow
-);
-    wire [WIDTH-1:0] carry;
+  );
+  wire [WIDTH-1:0] carry;
 
-    genvar i;
-    generate
-        for (i = 0; i < WIDTH; i = i + 1) begin : RCA
-            if (i == 0) begin
-                fullAdder FA (
+  genvar i;
+  generate
+    for (i = 0; i < WIDTH; i = i + 1)
+    begin : RCA
+      if (i == 0)
+      begin
+        fullAdder FA (
                     .a(a[i]), .b(b[i]), .cin(cin),
                     .sum(sum[i]), .cout(carry[i])
-                );
-            end else begin
-                fullAdder FA (
+                  );
+      end
+      else
+      begin
+        fullAdder FA (
                     .a(a[i]), .b(b[i]), .cin(carry[i-1]),
                     .sum(sum[i]), .cout(carry[i])
-                );
-            end
-        end
-    endgenerate
+                  );
+      end
+    end
+  endgenerate
 
-    assign cout = carry[WIDTH-1];
+  assign cout = carry[WIDTH-1];
 
-    assign overflow = (a[WIDTH-1] & b[WIDTH-1] & ~sum[WIDTH-1]) |
-                      (~a[WIDTH-1] & ~b[WIDTH-1] & sum[WIDTH-1]);
+  assign overflow = (a[WIDTH-1] & b[WIDTH-1] & ~sum[WIDTH-1]) |
+         (~a[WIDTH-1] & ~b[WIDTH-1] & sum[WIDTH-1]);
 endmodule
